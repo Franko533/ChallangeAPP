cbc26ef893801318f8e090a534d37da4abed0df2
Franek Śliwiński <frankosliwka5333@gmail.com>
2024-03-18 12:30:26 +0100

    zadanie domowe dzien 4

--- ChallangeApp/Program.cs
+++ ChallangeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Franek";
+var age = 15;
+var isMale = true;
+if (isMale == false)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("kobieta przed 30");
+    }
+    if (age == 30 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa lat 30");
+    }
+}
+if (isMale == true && age < 18)
+{
+    Console.WriteLine("niepelnoletni mezczyzna");
+}
